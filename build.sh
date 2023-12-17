6236cd6b575a4cc573bd285240e8940ccb351317
jasonkayzk <271226192@qq.com>
2023-12-17 18:41:09 +0800

    feat: deploy hello

---
+++ build.sh
@@ -0,0 +1,3 @@
+docker build -t jasonkay/java-hello-app:v1.0.0 .
+
+docker push jasonkay/java-hello-app:v1.0.0
---
+++ src/main/java/io/jasonkayzk/github/SpringBootDeployDemoApplication.java
@@ -0,0 +1,11 @@
+package io.jasonkayzk.github;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDeployDemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDeployDemoApplication.class, args);
+    }
+}
